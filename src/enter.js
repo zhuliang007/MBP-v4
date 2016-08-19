f97ae0a54aa3d3aaa441f2742391dd6b6d33b8a1
steavengong <842228597@qq.com>
2016-08-19 17:33:43 +0800

    divide  wxhead

--- src/enter.js
+++ src/enter.js
@@ -1,14 +1,15 @@
 require('!style!css!normalize.css');
+require('!style!css!./app_share/mbp/styles/base.css');
 const React = require('react');
 const ReactDom = require('react-dom');
-const App = require('./components/divide/index');
+const App = require('./components/wxHead/index');
 
 if (module.hot) {
     module.hot.accept();
 }
 
 ReactDom.render(
-    <App></App>,
+    <App type={'mbp'}></App>,
     document.getElementById('app')
 )
 
