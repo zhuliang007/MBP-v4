f241b57182225b199ea0a72c09a8d37aa0fa6559
steavengong <842228597@qq.com>
2016-08-19 16:49:58 +0800

    divide

--- src/enter.js
+++ src/enter.js
@@ -1,7 +1,7 @@
+require('!style!css!normalize.css');
 const React = require('react');
 const ReactDom = require('react-dom');
 const App = require('./components/divide/index');
-require('!style!css!normalize.css');
 
 if (module.hot) {
     module.hot.accept();
