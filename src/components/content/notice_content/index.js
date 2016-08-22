7a1cf5ea3407a940bd474cb325a2a9d46c76c905
steavengong <842228597@qq.com>
2016-08-22 17:15:06 +0800

    serve_page user_head  tab_bar tab_body notice_content

---
+++ src/components/content/notice_content/index.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Administrator on 2016/8/22.
+ */
+const React = require('react');
+const Util = require('../../../util/index');
+const NoticeContent = React.createClass({
+    initNotice:function(data){
+        let notice = '<div></div>';
+        if(data){
+            notice = data;
+        }
+        return notice;
+    },
+    render:function(){
+        return (
+            <div className='notice_content'>
+                <div dangerouslySetInnerHTML = {{__html:this.initNotice(this.props.data)}}></div>
+            </div>
+        )
+    }
+})
+module.exports = NoticeContent;
---
+++ src/components/content/serve_content/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2016/8/22.
+ */
+const React = require('react');
+const ServeContent = React.createClass({
+    render:function(){
+        return (
+            <div className='serve_content'>
+                Hello ServeContent
+            </div>
+        )
+    }
+})
+module.exports = ServeContent;
