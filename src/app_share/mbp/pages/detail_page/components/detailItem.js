bf79d4fb8d86c4291907ce2ed66142219eaa8c7c
zl_sam <zl_sam@163.com>
2016-08-25 09:56:13 +0800

    1
    1

--- src/app_share/mbp/pages/detail_page/components/detailItem.js
+++ src/app_share/mbp/pages/detail_page/components/detailItem.js
@@ -1,5 +1,6 @@
 const React = require("React");
 const DetailImgContext = require("./detailImgContext");
+const UWX = require('../../../../../wx/index');
 
 
 const detailItem=React.createClass({
@@ -7,8 +8,13 @@ const detailItem=React.createClass({
     const details = this.props.data;
     const detailList = details.objectData.imgTxtList;
 
+    var img = '';
     var listComps;
     if(detailList!=null&&detailList.length>0){
+      var attachments = detailList[0].attachments;
+      if(attachments!=null&&attachments.length>0){
+        img =  attachments[0].location;
+      }
       var j=0;
       listComps = detailList.map(function(detail){
         ++j;
@@ -17,6 +23,9 @@ const detailItem=React.createClass({
         attachments={detail.attachments}></DetailImgContext>
       }.bind(this));
     }
+
+    UWX.shareItems(details.objectData.title,details.objectData.contents,window.location.href,img);
+
     return(
       <div className="clearFloat" key="detail">
         <div className="detail-row detail-title">
